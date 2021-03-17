ccf3b2f9d508ac719fbdb13f605b34779e0d473f
Alex Chen <achen115@ucr.edu>
2021-03-16 20:09:11 -0700

    add push script

--- push.sh
+++ push.sh
@@ -1,1 +1,1 @@
-git config user.email "achen115@ucr.edu"; git config user.name "Alex Chen"; git add *; git commit -m $1; git push remote master;
+git config user.email "achen115@ucr.edu"; git config user.name "Alex Chen"; git add *; git commit -m $1; git pull remote master; git push remote master;
